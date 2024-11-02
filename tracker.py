0e8dcb4545aa3d437a77a192b2d0e129d12d407c
Tyler McGeorge <tylermcg@gmail.com>
2024-11-02 08:55:15 -0500

    day 2

--- tracker.py
+++ tracker.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
+import argparse
 import json
+from typing import Dict, Union
 
 
-class Result:
+class GameResult:
   ranks = dict()
   by_name = dict()
 
@@ -70,17 +72,28 @@ class Result:
     return self.vote_position is not None and 1 <= int(self.vote_position) <= 20
 
 
-class Vote:
-  votes = dict()
-  by_name = dict()
-  def __init__(self, position, name):
-    self.position = int(position)
+class GameVote:
+  current_year: Union[str, None] = None
+  votes: Dict[int, "GameVote"] = dict()
+  by_name: Dict[str, "GameVote"] = dict()
+
+  @classmethod
+  def set_current_year(cls, year: Union[int, str]) -> None:
+    cls.current_year = year
+
+  def __init__(self, name: str, votes: Dict[Union[int, str], Union[int, str]]) -> None:
+    self.position = votes[self.current_year]
     self.name = name
-    self.votes[self.position] = self
     self.by_name[name] = self
+    try:
+      self.votes[int(self.position)] = self
+    except:
+      pass
 
 
 class Markup:
+  BLOCK_START = ''
+  BLOCK_END = ''
   UNRANKED_COLOR = '#A4ACBA'
   PENDING_RANK = '#E3E3CA'
 
@@ -155,46 +168,6 @@ class Markup:
     return f'VOTED{count:>4}'
 
   @classmethod
-  def vote_position(cls, position, rank, game_name):
-    leader = cls.row_leader(rank)
-    trailer = cls.row_trailer(rank)
-    if rank:
-      result = f'{rank:>3}'
-    else:
-      result = '  ?'
-      game_name = ''.join([ '?' ] * len(game_name))
-    rank_result = f'[b]#{position:>2} ({result})[/b]'
-    return f'{leader}{rank_result}  {game_name.ljust(72 - len(rank_result))}{trailer}'
-
-  @classmethod
-  def owned_list(cls, rank, game_name):
-    leader = cls.row_leader(rank)
-    trailer = cls.row_trailer(rank)
-    name_format = item.ljust(61)
-    if len(name_format) > 61:
-      name_format = name_format[:58] + '...'
-    return f'║ {leader}#[b]{n:>3}[/b]  {name_format}{trailer} ║\n'
-
-  @classmethod
-  def wishlist(cls, rank, game_name, wishlist_priority):
-    leader = cls.row_leader(rank)
-    trailer = cls.row_trailer(rank)
-    name_format = game_name.ljust(58)
-    if len(name_format) > 58:
-      name_format = name_format[:55] + '...'
-    return f'║ {leader}#[b]{n:>3}[/b]  {name_format}({wishlist_priority}){trailer} ║\n'
-
-  @classmethod
-  def row_leader(cls, rank):
-    bgcolor = cls.bg_color(rank)
-    fgcolor = cls.text_color(bgcolor)
-    return f'[bgcolor={bgcolor}][color={fgcolor}]'
-
-  @classmethod
-  def row_trailer(cls, rank):
-    return f'[/color][/bgcolor]'
-
-  @classmethod
   def bg_color(cls, rank):
     if rank is None:
       return cls.PENDING_RANK
@@ -216,99 +189,222 @@ class Markup:
     lv = len(val)
     return tuple(int(val[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
 
-with open('data.json') as f:
-  data_raw = json.load(f)
+class BBCode(Markup):
+  BLOCK_START = '[c]'
+  BLOCK_END = '[/c]'
+  @classmethod
+  def vote_position(cls, position, rank, game_name):
+    leader = cls.row_leader(rank)
+    trailer = cls.row_trailer(rank)
+    if rank:
+      result = f'{rank:>3}'
+    else:
+      result = '  ?'
+      game_name = ''.join([ '?' ] * len(game_name))
+    rank_result = f'[b]#{position:>2} ({result})[/b]'
+    return f'{leader}{rank_result}  {game_name.ljust(72 - len(rank_result))}{trailer}'
 
-for rank, result in data_raw['top200'].items():
-  if rank == '':
-    continue
-  result.update( { "rank": rank } )
-  result['is_owned'] = result['own']
-  del result['own']
-  result['wishlist_priority'] = result['wishlist']
-  del result['wishlist']
-  if 'owned' in result:
-    result['owned_items'] = result['owned']
-    del result['owned']
-  Result(**result)
+  @classmethod
+  def owned_list(cls, rank, game_name):
+    leader = cls.row_leader(rank)
+    trailer = cls.row_trailer(rank)
+    name_format = game_name.ljust(61)
+    if len(name_format) > 61:
+      name_format = name_format[:58] + '...'
+    return f'║ {leader}#[b]{rank:>3}[/b]  {name_format}{trailer} ║\n'
 
-for position, game in data_raw['votes'].items():
-  Vote(position, game['name'])
-  if game['name'] in Result.by_name:
-    Result.by_name[game['name']].vote(position)
+  @classmethod
+  def wishlist(cls, rank, game_name, wishlist_priority):
+    leader = cls.row_leader(rank)
+    trailer = cls.row_trailer(rank)
+    name_format = game_name.ljust(58)
+    if len(name_format) > 58:
+      name_format = name_format[:55] + '...'
+    return f'║ {leader}#[b]{n:>3}[/b]  {name_format}({wishlist_priority}){trailer} ║\n'
 
-output = """[c]
+  @classmethod
+  def row_leader(cls, rank):
+    bgcolor = cls.bg_color(rank)
+    fgcolor = cls.text_color(bgcolor)
+    return f'[bgcolor={bgcolor}][color={fgcolor}]'
+
+  @classmethod
+  def row_trailer(cls, rank):
+    return f'[/color][/bgcolor]'
+
+class Markdown(Markup):
+  BLOCK_START = '<code>\n'
+  BLOCK_END = '</code>'
+  @classmethod
+  def vote_position(cls, position, rank, game_name):
+    raw_game_name = game_name
+    leader = cls.row_leader(rank)
+    trailer = cls.row_trailer(rank)
+    if rank:
+      result = f'{rank:>3}'
+    else:
+      result = '  ?'
+      game_name = ''.join([ '?​' ] * len(game_name))
+    rank_result = f'**#{position:>2} ({result})**'
+    return f'{leader}{rank_result}  {game_name.ljust(69 - len(rank_result) + len(game_name) - len(raw_game_name))}{trailer}'
+
+  @classmethod
+  def owned_list(cls, rank, game_name):
+    leader = cls.row_leader(rank)
+    trailer = cls.row_trailer(rank)
+    name_format = game_name.ljust(61)
+    if len(name_format) > 61:
+      name_format = name_format[:60] + '...'
+    return f'║ {leader}#**{rank:>3}**  {name_format}{trailer} ║\n'
+
+  @classmethod
+  def wishlist(cls, rank, game_name, wishlist_priority):
+    leader = cls.row_leader(rank)
+    trailer = cls.row_trailer(rank)
+    name_format = game_name.ljust(58)
+    if len(name_format) > 58:
+      name_format = name_format[:57] + '...'
+    return f'║ {leader}#**{n:>3}**  {name_format}({wishlist_priority}){trailer} ║\n'
+
+  @classmethod
+  def row_leader(cls, rank):
+    bgcolor = cls.bg_color(rank)
+    fgcolor = cls.text_color(bgcolor)
+    return f'<span style="color: {fgcolor}; background-color: {bgcolor}">'
+
+  @classmethod
+  def row_trailer(cls, rank):
+    return f'</span>'
+
+
+def main(args):
+  if args.format == "bbcode":
+    markup = BBCode
+  elif args.format == "markdown":
+    markup = Markdown
+  else:
+    print(f'WARNING: unknown format: {args.format}')
+    markup = BBCode
+  
+  with open(args.votes) as f:
+    data_raw_votes = json.load(f)
+
+  with open(args.results) as f:
+    data_raw_results = json.load(f)
+
+  current_year = data_raw_results['year']
+  GameVote.set_current_year(current_year)
+
+  for rank, result in data_raw_results['results'].items():
+    if rank == '':
+      continue
+    result.update( { "rank": rank } )
+    result['is_owned'] = result['own']
+    del result['own']
+    result['wishlist_priority'] = result['wishlist']
+    del result['wishlist']
+    if 'owned' in result:
+      result['owned_items'] = result['owned']
+      del result['owned']
+    GameResult(**result)
+
+  for game_name, game_data in data_raw_votes.items():
+    if 'vote_ranks' in game_data:
+      game: GameVote = GameVote(game_name, game_data['vote_ranks'])
+      if game_name in GameResult.by_name:
+        GameResult.by_name[game_name].vote(game.position)
+
+  output = f"""{markup.BLOCK_START}
 ╔═════════════════════════════════════════════════════════════════════╗
 ║                       PILLBOX'S GAME STATISTICS                     ║
 ╠═════════╤═══════════╤═══════════╤═══════════╤═══════════╤═══════════╣
 """
 
-for n in range(200, 1, -10):
-  if n not in Result.ranks:
-    continue
-  lo = n - 9
-  range_label = f'{n:>3}-{lo:>3}'
-  own_count = Result.count_owned(lo, n)
-  sold_count = Result.count_sold(lo, n)
-  want_count = Result.count_want(lo, n)
-  played_count = Result.count_played(lo, n)
-  voted_count = Result.count_voted(lo, n)
+  for n in range(200, 1, -10):
+    if n not in GameResult.ranks:
+      continue
+    lo = n - 9
+    range_label = f'{n:>3}-{lo:>3}'
+    own_count = GameResult.count_owned(lo, n)
+    sold_count = GameResult.count_sold(lo, n)
+    want_count = GameResult.count_want(lo, n)
+    played_count = GameResult.count_played(lo, n)
+    voted_count = GameResult.count_voted(lo, n)
 
-  row_markup_leader = Markup.row_leader(n)
-  range_markup = f'[b]{range_label}[/b]'
-  own_markup = Markup.own(own_count)
-  sold_markup = Markup.sold(sold_count)
-  want_markup = Markup.want(want_count)
-  played_markup = Markup.played(played_count)
-  voted_markup = Markup.voted(voted_count)
-  row_markup_trailer = Markup.row_trailer(n)
+    row_markup_leader = markup.row_leader(n)
+    range_markup = f'[b]{range_label}[/b]'
+    own_markup = markup.own(own_count)
+    sold_markup = markup.sold(sold_count)
+    want_markup = markup.want(want_count)
+    played_markup = markup.played(played_count)
+    voted_markup = markup.voted(voted_count)
+    row_markup_trailer = markup.row_trailer(n)
 
-  if n < 200 and n % 50 == 0:
-    output += '╠═════════╪═══════════╪═══════════╪═══════════╪═══════════╪═══════════╣\n'
-  output += f'║ {row_markup_leader}{range_markup} │ {own_markup} ┊ {sold_markup} ┊ {want_markup} ┊ {played_markup} ┊ {voted_markup}{row_markup_trailer} ║\n'
+    if n < 200 and n % 50 == 0:
+      output += '╠═════════╪═══════════╪═══════════╪═══════════╪═══════════╪═══════════╣\n'
+    output += f'║ {row_markup_leader}{range_markup} │ {own_markup} ┊ {sold_markup} ┊ {want_markup} ┊ {played_markup} ┊ {voted_markup}{row_markup_trailer} ║\n'
 
-output += '╠═════════╧═══════════╧═══════════╧═══════════╧═══════════╧═══════════╣\n'
+  output += '╠═════════╧═══════════╧═══════════╧═══════════╧═══════════╧═══════════╣\n'
 
-for v in range(1, 21):
-  v_game = Vote.votes[v]
-  if v_game.name in Result.by_name:
-    rank = Result.by_name[v_game.name].rank
-  else:
-    rank = None
-  markup = Markup.vote_position(v, rank, v_game.name)
-  output += f'║ {markup} ║\n'
+  for v in range(1, 21):
+    v_game = GameVote.votes[v]
+    if v_game.name in GameResult.by_name:
+      rank = GameResult.by_name[v_game.name].rank
+    else:
+      rank = None
+    out_markup = markup.vote_position(v, rank, v_game.name)
+    output += f'║ {out_markup} ║\n'
 
-output += '╠═════════════════════════════════════════════════════════════════════╣\n'
+  output += '╠═════════════════════════════════════════════════════════════════════╣\n'
 
-own_count = Result.count_owned()
-output += f'║                                                  OWN [b]{own_count:>3} / 200[/b]      ║\n'
+  own_count = GameResult.count_owned()
+  output += f'║                                                  OWN [b]{own_count:>3} / 200[/b]      ║\n'
 
-for n in range(200, 0, -1):
-  if n not in Result.ranks:
-    continue
-  result = Result.ranks[n]
-  if result.is_owned:
-    for item in result.items:
-      output += Markup.owned_list(n, item)
+  for n in range(200, 0, -1):
+    if n not in GameResult.ranks:
+      continue
+    result = GameResult.ranks[n]
+    if result.is_owned:
+      for item in result.items:
+        output += markup.owned_list(n, item)
 
-output += '╠═════════════════════════════════════════════════════════════════════╣\n'
+  output += '╠═════════════════════════════════════════════════════════════════════╣\n'
 
-wish_count = Result.count_want()
-output += f'║                                             WISHLIST [b]{wish_count:>3} / 200[/b]      ║\n'
+  wish_count = GameResult.count_want()
+  output += f'║                                             WISHLIST [b]{wish_count:>3} / 200[/b]      ║\n'
 
-for n in range(200, 0, -1):
-  if n not in Result.ranks:
-    continue
-  result = Result.ranks[n]
-  if result.wishlist_priority.lower() != "no":
-    wish_markup = 'M'
-    if result.wishlist_priority.lower() == 'high':
-      wish_markup = 'H'
-    if result.wishlist_priority.lower() == 'low':
-      wish_markup = 'L'
-    output += Markup.wishlist(n, result.name, wish_markup)
+  for n in range(200, 0, -1):
+    if n not in GameResult.ranks:
+      continue
+    result = GameResult.ranks[n]
+    if result.wishlist_priority.lower() != "no":
+      wish_markup = 'M'
+      if result.wishlist_priority.lower() == 'high':
+        wish_markup = 'H'
+      if result.wishlist_priority.lower() == 'low':
+        wish_markup = 'L'
+      output += markup.wishlist(n, result.name, wish_markup)
 
-output += '╚═════════════════════════════════════════════════════════════════════╝\n'
-output += '[/c]\n'
+  output += '╚═════════════════════════════════════════════════════════════════════╝\n'
+  output += f'{markup.BLOCK_END}\n'
+  
+  full_space = ' '
+  zero_space = '​​'
+  if args.format == 'markdown':
+    old_output = output
+    output = ''
+    for c in old_output:
+      if c == ' ':
+        output = f'{output}{full_space}'
+      else:
+        output = f'{output}{c}'
 
-print(output)
+  print(output)
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(prog='top20-tracker', description='Peoples Choice Top20 Results Tracker')
+  parser.add_argument('--votes', nargs='?', type=str, default="votes.json")
+  parser.add_argument('--results', nargs='?', type=str, default="results.json")
+  parser.add_argument('--format', type=str, default="bbcode", choices=["bbcode", "markdown"])
+  args = parser.parse_args()
+  main(args)
