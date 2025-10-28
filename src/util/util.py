d272cf349a55a894f0654ed51383ebcde9dba3ad
Tyler McGeorge <tylermcg@gmail.com>
2025-10-28 17:58:33 +0000

    everything seems to be working now

---
+++ src/util/util.py
@@ -0,0 +1,15 @@
+
+import argparse
+
+
+class Utility:
+    @staticmethod
+    def str2bool(arg: str) -> bool:
+        arg = arg.lower()
+        if arg in [ 'yes', 'y', 'true', 't', '1' ]:
+            return True
+        elif arg in [ 'no', 'n', 'false', 'f', '0' ]:
+            return False
+        else:
+            raise argparse.ArgumentTypeError("Invalid option, choose from: y, yes, t, true, n, no, f, false, 0, 1")
+
